7e108ae8e9c3e62b84fbc1e5e15fbdbda871110d
Evgeny <evgeny.gutz@yandex.ru>
2025-09-23 11:34:36 +0300

    feature: добавил отображение времени в окне браузера

--- app.js
+++ app.js
@@ -3,25 +3,19 @@
 function timerBeforeEndYear() {
     const finalDate = `${new Date().getFullYear() + 1}-01-01`;
     const end = new Date(finalDate).getTime();
+    const timerElement = document.getElementById('timer');
 
     setInterval(() => {
-        const month = Intl.DateTimeFormat('ru-RU', {
-            month: 'numeric',
-        }).format(end - new Date());
-        const day = Intl.DateTimeFormat('ru-RU', {
-                day: 'numeric',
-            }).format(end - new Date());
-        const hour = Intl.DateTimeFormat('ru-RU', {
-                hour: 'numeric',
-            }).format(end - new Date());
-        const minute = Intl.DateTimeFormat('ru-RU', {
-                minute: 'numeric',
-            }).format(end - new Date());
-        const second = Intl.DateTimeFormat('ru-RU', {
-                second: 'numeric',
-            }).format(end + 100 - new Date());
-        console.clear();
-        console.log(`${month} месяцев, ${day} дней, ${hour} часов, ${minute} минут, ${second} секунд, `);
+        let currentDate = end + 100 - new Date();
+
+        const month = Intl.DateTimeFormat('ru-RU', {month: 'numeric'}).format(currentDate);
+        const day = Intl.DateTimeFormat('ru-RU', {day: 'numeric'}).format(currentDate);
+        const hour = Intl.DateTimeFormat('ru-RU', {hour: 'numeric'}).format(currentDate);
+        const minute = Intl.DateTimeFormat('ru-RU', {minute: 'numeric'}).format(currentDate);
+        const second = Intl.DateTimeFormat('ru-RU', {second: 'numeric'}).format(currentDate);
+
+        timer.innerHTML = `${month} месяцев, ${day} дней, ${hour} часов, ${minute} минут, ${second} секунд`;
+        
     }, 1000);
     
 }
