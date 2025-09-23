807af208c6551e15547034b6cea65daa53418b9c
Evgeny <evgeny.gutz@yandex.ru>
2025-09-23 11:40:21 +0300

    refactor: исправление неточностей

--- app.js
+++ app.js
@@ -3,10 +3,10 @@
 function timerBeforeEndYear() {
     const finalDate = `${new Date().getFullYear() + 1}-01-01`;
     const end = new Date(finalDate).getTime();
-    const timerElement = document.getElementById('timer');
-
+    
     setInterval(() => {
-        let currentDate = end + 100 - new Date();
+        const timerElement = document.getElementById('timer');
+        let currentDate = end + 100 - Date.now();
 
         const month = Intl.DateTimeFormat('ru-RU', {month: 'numeric'}).format(currentDate);
         const day = Intl.DateTimeFormat('ru-RU', {day: 'numeric'}).format(currentDate);
@@ -14,10 +14,12 @@ function timerBeforeEndYear() {
         const minute = Intl.DateTimeFormat('ru-RU', {minute: 'numeric'}).format(currentDate);
         const second = Intl.DateTimeFormat('ru-RU', {second: 'numeric'}).format(currentDate);
 
-        timer.innerHTML = `${month} месяцев, ${day} дней, ${hour} часов, ${minute} минут, ${second} секунд`;
-        
+        timerElement.innerHTML = `${month} месяцев, ${day} дней, ${hour} часов, ${minute} минут, ${second} секунд`;
+
     }, 1000);
     
 }
+
+console.log();
 
 timerBeforeEndYear();
