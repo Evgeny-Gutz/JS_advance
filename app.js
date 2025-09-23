0731f4297fbbd36ea8851771570edc16bf27f036
Evgeny <evgeny.gutz@yandex.ru>
2025-09-23 10:36:59 +0300

    чистка

--- app.js
+++ app.js
@@ -1,15 +1,1 @@
 'use strict'
-
-const data = [
-    { id: 1, name: 'Вася' },
-    { id: 2, name: 'Петя' },
-    { id: 1, name: 'Вася' },
-];
-
-
-
-let a = new Set(data.map((element, i, arr) => {
-            return arr.find(el => el.id === element.id);
-        }));
-
-console.log(a);
