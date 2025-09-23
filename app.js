10e885d4e20fbba7794ac6223990b0392a3d9225
Evgeny <evgeny.gutz@yandex.ru>
2025-09-23 12:01:37 +0300

    refactor: исправлениу Intl

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
 'use strict'
 
 function timerBeforeEndYear() {
-    const finalDate = `${new Date().getFullYear() + 1}-01-01`;
-    const end = new Date(finalDate).getTime();
+    const finalDate = new Date(`${new Date().getFullYear() + 1}-01-01`);
+    
+    const msInSecond = 1000;
+    const msInMinute = msInSecond * 60;
+    const msInHour = msInMinute * 60;
+    const msInDay = msInHour * 24;
+    const msInMonth = msInDay * 30; // приблизительно
     
     setInterval(() => {
         const timerElement = document.getElementById('timer');
-        let currentDate = end + 100 - Date.now();
+        const now = new Date();
+        let currentDate = finalDate - now;
 
-        const month = Intl.DateTimeFormat('ru-RU', {month: 'numeric'}).format(currentDate);
-        const day = Intl.DateTimeFormat('ru-RU', {day: 'numeric'}).format(currentDate);
-        const hour = Intl.DateTimeFormat('ru-RU', {hour: 'numeric'}).format(currentDate);
-        const minute = Intl.DateTimeFormat('ru-RU', {minute: 'numeric'}).format(currentDate);
-        const second = Intl.DateTimeFormat('ru-RU', {second: 'numeric'}).format(currentDate);
+        const month = Math.floor(currentDate / msInMonth);
+        const day = Math.floor((currentDate % msInMonth) / msInDay);
+        const hour = Math.floor((currentDate % msInDay) / msInHour);
+        const minute = Math.floor((currentDate % msInHour) / msInMinute);
+        const second = Math.floor((currentDate % msInMinute) / msInSecond);
 
-        timerElement.innerHTML = `${month} месяцев, ${day} дней, ${hour} часов, ${minute} минут, ${second} секунд`;
+        timerElement.textContent = `${month} месяцев, ${day} дней, ${hour} часов, ${minute} минут, ${second} секунд`;
 
     }, 1000);
     
 }
 
-console.log();
 
 timerBeforeEndYear();
